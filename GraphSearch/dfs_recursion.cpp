6696fcb44605d2b7bc372cb147d88379698b68da
Yizhou Sheng <yizhoush@usc.edu>
2019-03-18 17:18:11 -0700

    Added a Makefile and some test cases

--- GraphSearch/dfs_recursion.cpp
+++ GraphSearch/dfs_recursion.cpp
@@ -24,6 +24,7 @@ bool dfs_recursion(Node* start, Node* end)
 
 TEST_CASE("Basic Test", "[dfs_recursion]")
 {
+    Node::cnt = 0;
     SECTION("Really basic positive case") {
         Node::cnt = 0;
         Node A, B, C, D;
@@ -40,5 +41,39 @@ TEST_CASE("Basic Test", "[dfs_recursion]")
         B.neighbors = { &A, &C };
         C.neighbors = { &B };
         REQUIRE(dfs_recursion(&A, &D) == false);
+    }
+    SECTION("Octopus") {
+        Node A, B, C, D, E, F, G, H, I;
+        A.neighbors = { &B, &D, &F, &H };
+        B.neighbors = { &A, &C };
+        C.neighbors = { &B };
+        D.neighbors = { &A, &E };
+        E.neighbors = { &D };
+        F.neighbors = { &A, &G };
+        G.neighbors = { &F };
+        H.neighbors = { &A, &I };
+        I.neighbors = { &H };
+
+        REQUIRE(dfs_recursion(&A, &I) == true);
+    }
+}
+
+TEST_CASE("Somewhat edge cases", "dfs_recursion")
+{
+    Node::cnt = 0;
+    SECTION("Small loop") {
+        Node A, B, C, D;
+        A.neighbors = { &B, &C };
+        B.neighbors = { &A, &C };
+        C.neighbors = { &B, &C };
+
+        REQUIRE(dfs_recursion(&A, &D) == true);
+    }
+
+    SECTION("Self-Loop") {
+        Node A, B;
+        A.neighbors = { &A };
+        
+        REQUIRE(dfs_recursion(&A, &B) == false);
     }
 }
